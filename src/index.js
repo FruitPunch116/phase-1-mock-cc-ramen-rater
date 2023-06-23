97fc409ff873c38d8fd3f856b9b51ed737b4f05a
Angel Figueroa <gabrielfigue4120@gmail.com>
2023-06-23 00:03:28 -0400

    bugged code

--- src/index.js
+++ src/index.js
@@ -1,49 +1,68 @@
-// write your code here
-const URL = "http://localhost:3000/ramens"
+// //Declare the variable URL.
+// const URL = "http://localhost:3000/ramens"
 
-fetch(URL)
-.then(resp => resp.json())
-.then(ramens => {
-    ramens.forEach(ramen=>renderRamen(ramen))
-})
-// .then(data => console.log(data))
+// //GET request to the database, to display data on the browser.
+// fetch(URL)
+// .then(resp => resp.json())
+// .then(ramens => {
+//     ramens.forEach(ramen=>renderRamen(ramen))
+//     renderRamenDetails(ramens[0])
+// })
+// // .then(data => console.log(data))
 
+// const form = document.querySelector("#new-ramen")
+// form.addEventListener("submit", updatedComments)
 
-function renderRamen(ramen) {
-    const ramenMenu = document.querySelector("#ramen-menu");
+// function renderRamen(ramen) {
+//     const ramenMenu = document.querySelector("#ramen-menu");
     
-    const image = document.createElement("img");
-    image.src = ramen.image
-    ramenMenu.append(image)
+//     const image = document.createElement("img");
+//     image.src = ramen.image;
+//     ramenMenu.append(image);
     
-    image.addEventListener("click", () => {
-        const ramenDetail = document.querySelector(".detail-image");
-        ramenDetail.src = ramen.image;
-        ramenDetail.alt = ramen.image;
-        //Get the values from
-        document.querySelector(".name").textContent = ramen.name;
-        document.querySelector(".restaurant").textContent = ramen.restaurant;
-        document.querySelector("#rating-display").textContent = ramen.rating;
-        document.querySelector("#comment-display").textContent = ramen.comment;
-    })
-    
-}
+//     //When the image is clicked displays the ramen details.
+//     //The render ramen details wirks as the callback for the eventen listener palced at the image.
+//     image.addEventListener("click", (ramen) => renderRamenDetails (ramen));
+// }
 
-// Updates the form
-const form = document.querySelector("#new-ramen")
-form.addEventListener("submit", event =>{
-    event.preventDefault()
-    const newRamenName = event.target.name.value;
-    const newRamenRestaurant = event.target.restaurant.value;
-    const newRamenRating = event.target.rating.value;
-    const newRamenImage = event.target.image.value;
-    const newRamenComment = event.target["new-comment"].value;
-    const newRamen = {
-        "name": newRamenName,
-        "restaurant": newRamenRestaurant,
-        "rating": newRamenRating,
-        "comment": newRamenComment,
-        "image": newRamenImage
-    }
-    renderRamen(newRamen);
-});
+// function renderRamenDetails (ramen) {
+//     const ramenDetail = document.querySelector(".detail-image");
+//     const image = document.createElement("img");
+//     image.src = ramen.image;
+//     image.alt = ramen.image;
+//     ramenDetail.append(image)
+//     //Declaration process.
+//     //Get the values from the database.
+//     const name = document.querySelector(".name")
+//     name.textContent = ramen.name;
+//     const restaurant = document.querySelector(".restaurant")
+//     restaurant.textContent = ramen.restaurant;
+//     const rating =document.querySelector("#rating-display")
+//     rating.textContent = ramen.rating;
+//     const comment = document.querySelector("#comment-display")
+//     comment.textContent = ramen.comment;
+// }
+
+
+// // Updates the form
+// function newRamen (event) {
+//     event.preventDefault()
+//     //Selects the ID as the target and assign the value as the content.
+//     const newRamenName = event.target.name.value;
+//     const newRamenRestaurant = event.target.restaurant.value;
+//     const newRamenRating = event.target.rating.value;
+//     const newRamenImage = event.target.image.value;
+//     //Give the comments a new value using the comment id
+//     const newRamenComment = event.target["new-comment"].value;
+//     const newRamen = {
+//         "name": newRamenName,
+//         "restaurant": newRamenRestaurant,
+//         "rating": newRamenRating,
+//         "comment": newRamenComment,
+//         "image": newRamenImage
+//     }
+//     renderRamen(newRamen)
+// }
+
+
+//code need to be fixed to make it work.
